48a0f2df83d54461d16c584b3fa973442c4e26b1
DanielKnoll <knoll.dani@gmail.com>
2017-07-27 16:45:26 +0200

    Clutter and PEP8 errors removed

--- bowling.py
+++ bowling.py
@@ -7,9 +7,8 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
+
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
@@ -18,17 +17,22 @@ def score(game):
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
+
         last = get_value(game[i])
+
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+
+        if in_first_half is True:
             in_first_half = False
         else:
             in_first_half = True
+
         if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
     return result
+
 
 def get_value(char):
     if char == '1' or char == '2' or char == '3' or \
