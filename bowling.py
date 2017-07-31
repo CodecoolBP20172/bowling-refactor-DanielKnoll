ad776421ef02f0655ca099c28ef46f82d21da046
DanielKnoll <knoll.dani@gmail.com>
2017-07-31 14:14:35 +0200

    More renaming, added spare handler function.

--- bowling.py
+++ bowling.py
@@ -1,26 +1,30 @@
+def isspare(result, point, last_point, max_point):
+    if point == '/':
+        result += max_point - last_point
+    else:
+        result += get_value(point)
+    return result
+
+
 def score(game):
     result = 0
     current_frame = 1
     max_frame = 10
-    max_score = 10
+    max_point = 10
     first_roll = True
     for i in range(len(game)):
-        if game[i] == '/':
-            result += max_score - last
-        else:
-            result += get_value(game[i])
+        result = isspare(result, game[i], get_value(game[i-1]), max_point)
 
-        if current_frame < max_frame and get_value(game[i]) == max_score:
+        if current_frame < max_frame and game[i].lower() in "x/":
             if game[i].lower() == '/':
                 result += get_value(game[i+1])
             elif game[i].lower() == 'x':
                 result += get_value(game[i+1])
                 if game[i+2] == '/':
-                    result += max_score - get_value(game[i+1])
+                    result += max_point - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
-
-        last = get_value(game[i])
+                # isspare(result, game[i+2], get_value(game[i+1]), max_point)
 
         if first_roll:
             first_roll = False
@@ -34,14 +38,14 @@ def score(game):
     return result
 
 
-def get_value(score):
-    max_score = 10
-    min_score = 0
-    if score.isdigit() and (min_score < int(score) < max_score):
-        return int(score)
-    elif score.lower() in 'x/':
-        return max_score
-    elif score == '-':
-        return min_score
+def get_value(point):
+    max_point = 10
+    min_point = 0
+    if point.isdigit() and (min_point < int(point) < max_point):
+        return int(point)
+    elif point.lower() in 'x/':
+        return max_point
+    elif point == '-':
+        return min_point
     else:
         raise ValueError()
